f384ff39bb5b62bda682eb3ab2b8444f8634edbc
Nixenos <janota241@gmail.com>
2017-03-24 21:31:26 +0100

    added morse scratch

---
+++ morse/mor.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+char morse[25]; 
+int main(){
+    morse[0]='.-'; //a
+    morse[1]='-...'; //b
+    morse[2]='-.-.'; //c
+    morse[3]='-..'; //d  
+    morse[4]='.'; //e
+    morse[5]='..-.'; //f
+    morse[6]='--.'; //g
